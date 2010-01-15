bea6ae2b3afb42b7c11c9a4175958518aa836406
Willie Walker <william.walker@sun.com>
2010-01-15 12:09:46 -0500

    Fix for bgo#607077 - pyatspi2: magic to detect whether to use corba version or not can be broken

--- pyatspi/__init__.py
+++ pyatspi/__init__.py
@@ -13,6 +13,8 @@
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 try:
+    gconf = None
+    gconfClient = None
     import gconf
     gconfClient = gconf.client_get_default()
     useCorba = gconfClient.get_bool("/desktop/gnome/interface/at-spi-corba")
