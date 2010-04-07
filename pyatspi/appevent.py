42d9676bd12c78c2880e87b776a52c566c551228
Mike Gorse <mgorse@novell.com>
2010-04-07 11:28:03 -0400

    Correct marshalling of object:property-change:accessible-parent

--- pyatspi/appevent.py
+++ pyatspi/appevent.py
@@ -199,7 +199,7 @@ def event_type_to_signal_reciever(bus, factory, event_handler, event_type):
                 if   type.is_subtype (EventType ("object:bounds-changed")):
                         any_data = BoundingBox(*any_data)
                 elif (type.is_subtype (EventType ("object:children-changed")) or
-                      type.is_subtype (EventType ("object:property-change:parent"))):
+                      type.is_subtype (EventType ("object:property-change:accessible-parent"))):
                         data_name, data_path = any_data;
                         any_data = factory (data_name, data_path, interfaces.ATSPI_ACCESSIBLE)
 
