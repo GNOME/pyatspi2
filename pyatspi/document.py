3981be0e0ae3ebbc111448525fe548d2d6806aec
Mike Gorse <mgorse@novell.com>
2011-05-04 10:27:29 -0500

    Fix traceback when calling document functions

--- pyatspi/document.py
+++ pyatspi/document.py
@@ -14,7 +14,7 @@
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 from gi.repository import Atspi
-import utils
+from utils import *
 
 __all__ = [
            "Document",
