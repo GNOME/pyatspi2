b37993d5d16ec9b688dc57158d12c6a82b098e29
Bjørn Lie <bjorn.lie@gmail.com>
2012-09-18 20:21:17 +0200

    pyatspi2: Fix incorrect-fsf-address in various files
    The Free Software Foundation address in these files are outdated, this
    patch updates the address.

--- pyatspi/utils.py
+++ pyatspi/utils.py
@@ -11,7 +11,7 @@
 #GNU General Public License for more details.
 #You should have received a copy of the GNU Lesser General Public License
 #along with this program; if not, write to the Free Software
-#Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 #Portions of this code originally licensed and copyright (c) 2005, 2007
 #IBM Corporation under the BSD license, available at
