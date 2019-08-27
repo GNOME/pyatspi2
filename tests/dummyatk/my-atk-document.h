bcc2c0df75be3cb3c8c102d81c8744849cf4c17e
Mike Gorse <mgorse@alum.wpi.edu>
2019-08-27 09:58:45 -0500

    Fix some copyrights, and update FSF address

--- tests/dummyatk/my-atk-document.h
+++ tests/dummyatk/my-atk-document.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Mike Gorse <mgorse@novell.com>
+ * Copyright 2010 Novell, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -13,8 +13,8 @@
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 #ifndef MY_ATK_DOCUMENT_H
