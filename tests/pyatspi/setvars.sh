bbd5451cfac54ded1d2094ebdc10dc3ef06d52a9
Mike Gorse <mgorse@suse.com>
2025-06-28 17:10:59 -0500

    Remove unused script

--- tests/pyatspi/setvars.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH=../../
-
-export TEST_DATA_DIRECTORY=../data
-export TEST_ATSPI_LIBRARY=../../atk-adaptor/libspiatk.so
-export TEST_MODULES_DIRECTORY=../apps
-export TEST_APPLICATION=../apps/test-application
